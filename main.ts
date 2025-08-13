33a215bae506bcc6f3633f01b97075c9ac769585
Ken Hu <silvesthu@gmail.com>
2025-08-13 15:50:34 +0900

    First version

--- main.ts
+++ main.ts
@@ -1,134 +1,111 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+// main.ts
+import { Plugin } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
-}
-
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
-
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class FullPathSearchResultsPlugin extends Plugin {
+	// We'll use a MutationObserver to watch for changes in the DOM,
+	// specifically when search results are loaded or updated.
+	private searchObserver: MutationObserver | null = null;
 
 	async onload() {
-		await this.loadSettings();
+		console.log('Loading Full Path Search Results plugin');
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
+		// Wait until the Obsidian layout is ready before attempting to modify the DOM.
+		// This ensures the search pane elements exist.
+		this.app.workspace.onLayoutReady(() => {
+			this.setupSearchObserver();
 		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
 	onunload() {
-
+		console.log('Unloading Full Path Search Results plugin');
+		this.disconnectSearchObserver();
 	}
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+	/**
+	 * Sets up the MutationObserver to watch the search results container.
+	 */
+	private setupSearchObserver() {
+		// Find the search results container. This CSS class is based on Obsidian's current DOM.
+		// It might change in future Obsidian versions.
+		const searchResultsContainer = document.querySelector('.search-results-children');
+
+		if (searchResultsContainer) {
+			this.searchObserver = new MutationObserver((mutationsList, observer) => {
+				for (const mutation of mutationsList) {
+					// Check if new nodes (search results) were added
+					if (mutation.type === 'childList' && mutation.addedNodes.length > 0) {
+						this.processSearchResults(mutation.addedNodes);
+					}
+				}
+			});
+
+			// Start observing for child list changes (new results being added)
+			this.searchObserver.observe(searchResultsContainer, { childList: true, subtree: true });
+			console.log('MutationObserver for search results set up.');
+
+			// Also process existing results if the plugin loads after a search has already been performed
+			this.processSearchResults(searchResultsContainer.querySelectorAll('.search-result'));
+		} else {
+			console.warn('Search results container not found. Plugin might not function correctly.');
+			// Retry setup after a short delay if not found immediately, in case it's a timing issue
+			setTimeout(() => this.setupSearchObserver(), 1000);
+		}
 	}
 
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
+	/**
+	 * Disconnects the MutationObserver to clean up resources.
+	 */
+	private disconnectSearchObserver() {
+		if (this.searchObserver) {
+			this.searchObserver.disconnect();
+			this.searchObserver = null;
+			console.log('MutationObserver for search results disconnected.');
+		}
 	}
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
+	/**
+	 * Processes a list of DOM nodes, adding file paths to search results.
+	 * @param nodes The NodeList or Array of nodes to process.
+	 */
+	private processSearchResults(nodes: NodeListOf<Element> | Node[]) {
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+		// console.log('processSearchResults');
 
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
+		nodes.forEach(node => {
 
-	display(): void {
-		const {containerEl} = this;
+			if (!(node instanceof Element))
+				return;
 
-		containerEl.empty();
+			var element = node.querySelector('.search-result-file-title')
+			if (!element)
+				return;
 
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+			const proto = app.workspace.getLeaf(element);
+			const origOpen = proto.openFile;
+
+			// console.log("hook");
+			// console.log(element);
+
+			proto.openFile = function(file: TFile, ...args: any[]) {
+
+				// console.log("restored");
+				proto.openFile = origOpen;
+
+				function extractParentFolder(path: string): string | null {
+					const parts = path.split('/');
+					if (parts.length >= 2) {
+						return parts[parts.length - 2] + " / ";
+					}
+					return "";
+				}
+
+				// console.log(file.path);
+
+				var innerElement = element.querySelector('.tree-item-inner');
+				if (!innerElement.textContent.includes("/"))
+					innerElement.textContent = extractParentFolder(file.path) + innerElement.textContent;
+			};
+			element.click();
+		});
 	}
 }
